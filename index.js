d2324b71ccecacb80671e4659b5fe8398520f8ac
aquaform <aquaform@yandex.ru>
2020-06-05 09:50:30 +0500

    tech commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  scalar DateTime
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -14,7 +16,18 @@ const typeDefs = `
     url: String!
     name: String!
     description: String
-    category: PhotoCategory!    
+    category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
+    created: DateTime!
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   type Query {
@@ -31,10 +44,45 @@ const typeDefs = `
   type Mutation {
     postPhoto(input: PostPhotoInput!): Photo!
   }
-`
+`;
 
-let _id = 0
-let photos = []
+let _id = 0;
+
+let users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Goga Plake" },
+  { githubLogin: "sSchmidt", name: "Serega Schmidt" }
+];
+
+let photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake"
+  },
+  {
+    id: "2",
+    name: "Enjoy the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt"
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt"
+  }
+];
+
+let tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" }
+];
 
 const resolvers = {
   Query: {
@@ -47,22 +95,41 @@ const resolvers = {
       let newPhoto = {
         id: _id++,
         ...args.input
-      }
-      photos.push(newPhoto)
-      return newPhoto
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     }
   },
 
   Photo: {
-    url: parent => `http://yousite.com/img/${parent.id}.jpg`
+    url: parent => `http://yousite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser);
+    },
+    taggedUsers: parent =>
+      tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID))
+  },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin);
+    },
+    inPhotos: parent =>
+      tag
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID))
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
+});
 
 server
   .listen()
-  .then(({url}) => console.log(`GraphQL Service running on ${url}`))
+  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
